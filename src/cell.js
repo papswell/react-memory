f88a95e2b5caf5ef010b4a0b189c87e22bd07fef
Pierre Guillaume <pierre@tricy.io>
2017-10-15 12:49:40 +0200

    styles improvments

--- src/cell.js
+++ src/cell.js
@@ -19,15 +19,20 @@ export default class Cell extends React.Component {
   render() {
     const { value, selected, disabled } = this.props;
 
-    const className = ''
+    const className = 'flip-container'
       .concat(selected ? ' selected' : '')
       .concat(disabled ? ' disabled' : '');
 
     return (
       <td onClick={this.handleClick} className={className}>
-        <span className="value">
-          { value }
-        </span>
+        <div className="flipper value">
+      		<div className="front">
+
+      		</div>
+      		<div className="back">
+            { value }
+      		</div>
+      	</div>
       </td>
     );
   }
